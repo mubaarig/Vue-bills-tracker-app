b7f444136d3131d3ae2e0533ecddd1c773bdedd0
Mubarig Cisman <mubo1@gmail.com>
2022-04-28 23:53:29 +0200

    initial commit

---
+++ src/app.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <NavBar />
+      <p>NavBar</p>
+    <div class="container flex">
+      <div class="w-1/2">
+        <BillsTable />
+      </div>
+      <div class="w-1/2">
+        <Chart :bills="activeBills" />
+      </div>
+    </div>
+  </main>
+</template>
+
+
+<script>
+  import Vue from "vue"
+
+  import AddCategory from "./components/AddCategory.vue"
+  import AddBill from "./components/AddBill.vue"
+  import NavBar from "./components/NavBar.vue"
+  import Chart from "./components/Chart.vue"
+  import BillsTable from "./components/BillsTable.vue"
+
+  export default {
+    name: "app",
+    components: {
+      AddCategory,
+      AddBill,
+      Chart,
+      BillsTable,
+      NavBar,
+    },
+  }
+</script>
